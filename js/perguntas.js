fb9648904f7c41529d761d8557a26290154506db
larinatsumi <129507719+larinatsumi@users.noreply.github.com>
2024-09-09 08:22:41 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,59 @@
+export const perguntas = [
+    {
+        enunciado: "A principal fonte de energia da matriz elétrica no Brasil e a que mais gera energia elétrica no mundo é?",
+        alternativas: [
+            {
+                texto: "Hidrelétrica!",
+                afirmacao: [
+                    "No caso do Brasil, a maior parte da energia elétrica é produzida nas hidrelétricas.Ela é responsável por boa parte da energia elétrica utilizada na região sudeste do Brasil.",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Hidrelétrica",
+                afirmacao: [
+                    "No caso do Brasil, a maior parte da energia elétrica é produzida nas hidrelétricas.Ela é responsável por boa parte da energia elétrica utilizada na região sudeste do Brasil.",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual das seguintes fontes de energia é considerada renovável?",
+        alternativas: [
+            {
+                texto: "Energia solar",
+                afirmacao: [
+                    "É renovável. Utiliza a luz do sol, que é uma fonte abundante e praticamente inesgotável a curto prazo, e não emite gases de efeito estufa durante a geração de energia.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Energia solar",
+                afirmacao: [
+                    "É renovável. Utiliza a luz do sol, que é uma fonte abundante e praticamente inesgotável a curto prazo, e não emite gases de efeito estufa durante a geração de energia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual das alternativas abaixo é uma desvantagem significativa da energia nuclear?",
+        alternativas: [
+            {
+                texto: "Resíduos radioativos.",
+                afirmacao: [
+                    "Esta é uma desvantagem significativa da energia nuclear. O gerenciamento de resíduos radioativos é um problema persistente e complexo, exigindo soluções de longo prazo para armazenamento seguro."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Resíduos radioativos",
+                afirmacao: [
+                    "Esta é uma desvantagem significativa da energia nuclear. O gerenciamento de resíduos radioativos é um problema persistente e complexo, exigindo soluções de longo prazo para armazenamento seguro."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+];
